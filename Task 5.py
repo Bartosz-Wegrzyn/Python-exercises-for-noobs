5445d74fc63af8ab0f707ace5fb00a12b735acd4
Bartosz-Wegrzyn <59071939+Bartosz-Wegrzyn@users.noreply.github.com>
2020-08-13 15:26:01 +0200

    Task 5 done

---
+++ Task 5.py
@@ -0,0 +1,21 @@
+# Take two lists, say for example these two:
+#
+#   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#   b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+def common_list(list1, list2):
+    list3 = []
+    for i in list1:
+            if i in list2:
+                if i in list3:
+                    continue
+                else:
+                    list3.append(i)
+    return list3
+
+print(common_list(a,b))
